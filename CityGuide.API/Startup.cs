546560d7e5eaac847503d57530bb30079e80f7e9
Vlad <v.rozanoff@gmail.com>
2020-05-14 21:47:58 +0300

    Add Cities controller

--- CityGuide.API/Startup.cs
+++ CityGuide.API/Startup.cs
@@ -15,6 +15,8 @@ namespace CityGuide.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +32,8 @@ namespace CityGuide.API
                 app.UseExceptionHandler();
             }
 
-            app.Run( (context) =>
-            {
-                throw new Exception("Example exception");
-                //await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+
         }
     }
 }
