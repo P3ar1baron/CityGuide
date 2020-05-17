0b3539b1c9f730ac869634f14f89edfb90e88288
Vlad <v.rozanoff@gmail.com>
2020-05-18 01:27:49 +0300

    Add creation dto Add create controller method

--- CityGuide.API/Controllers/PointsOfInterestController.cs
+++ CityGuide.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityGuide.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 namespace CityGuide.API.Controllers
@@ -21,7 +22,7 @@ namespace CityGuide.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities
@@ -43,7 +44,34 @@ namespace CityGuide.API.Controllers
             return Ok(pointOfInterest);
         }
 
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
+            if (city == null)
+            {
+                return NotFound();
+            }
 
+            //to be improved
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                        c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute(
+                "GetPointOfInterest",
+                new { cityId, id = finalPointOfInterest.Id },
+                finalPointOfInterest);
+        }
     }
 }
