4bf22a88b516778cbb1ad462a93abf4986c96cdc
Vlad <v.rozanoff@gmail.com>
2020-05-19 20:19:15 +0300

    Add appsettings.json Refactor LocalMailService and CloudMailService

--- CityGuide.API/Services/CloudMailService.cs
+++ CityGuide.API/Services/CloudMailService.cs
@@ -1,16 +1,22 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
 
 namespace CityGuide.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private readonly IConfiguration _configuration;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
 
         public void Send(string subject, string message)
         {
             //send mail - output to debug window
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Mail from {_configuration["mailSettings:mailFromAddress"]} to {_configuration["mailSettings:mailToAddress"]}, with CloudMailService.");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message: {message}");
         }
