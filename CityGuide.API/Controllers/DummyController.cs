c41c000538cc1b46a4b1fd4ac134105d13f6f176
Vlad <v.rozanoff@gmail.com>
2020-05-20 20:59:46 +0300

    Add Dummy controller

---
+++ CityGuide.API/Controllers/DummyController.cs
@@ -0,0 +1,27 @@
+﻿using CityGuide.API.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityGuide.API.Controllers
+{
+    [ApiController]
+    [Route("api/testdatabase")]
+    public class DummyController : ControllerBase
+    {
+        private readonly CityInfoContext _ctx;
+
+        public DummyController(CityInfoContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        [HttpGet]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
