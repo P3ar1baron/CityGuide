f505c3cc8566fe7dfa03c65ff0af21e8217d8865
Vlad <v.rozanoff@gmail.com>
2020-05-21 00:03:47 +0300

    Add code for seed in context class

--- CityGuide.API/Migrations/20200520210217_SeedData.Designer.cs
+++ CityGuide.API/Migrations/20200520210217_SeedData.Designer.cs
@@ -3,14 +3,16 @@ using CityGuide.API.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CityGuide.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20200520210217_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
