d1e5abfd0978de2a7f4377dc91b2ea9cba763364
Vlad <v.rozanoff@gmail.com>
2020-05-21 12:37:59 +0300

    Add CityInfoRepository with injected context

---
+++ CityGuide.API/Services/CityInfoRepository.cs
@@ -0,0 +1,39 @@
+﻿using CityGuide.API.Contexts;
+using CityGuide.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityGuide.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IQueryable<City> GetCities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public City GetCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
