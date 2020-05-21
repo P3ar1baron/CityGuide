18e16c292101cdd7caf4aa91ac439c93068347d9
Vlad <v.rozanoff@gmail.com>
2020-05-21 12:31:31 +0300

    Add ICityInfoRepository

---
+++ CityGuide.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,19 @@
+﻿using CityGuide.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityGuide.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        IQueryable<City> GetCities();
+
+        City GetCity(int cityId);
+
+        IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId);
+
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+    }
+}
