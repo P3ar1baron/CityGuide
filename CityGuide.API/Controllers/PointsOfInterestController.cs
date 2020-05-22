742a4f04a29a3aec5237d46f49e571c6c72dbee3
Vlad <v.rozanoff@gmail.com>
2020-05-22 14:12:57 +0300

    remove mapping code

--- CityGuide.API/Controllers/PointsOfInterestController.cs
+++ CityGuide.API/Controllers/PointsOfInterestController.cs
@@ -51,19 +51,7 @@ namespace CityGuide.API.Controllers
 
                 var pointsOfInterestForCity = _cityInfoRepository.GetPointsOfInterestForCity(cityId);
 
-                var pointsOfInterestForCityResults = new List<PointOfInterestDto>();
-
-                foreach (var poi in pointsOfInterestForCity)
-                {
-                    pointsOfInterestForCityResults.Add(new PointOfInterestDto()
-                    {
-                        Id = poi.Id,
-                        Name = poi.Name,
-                        Description = poi.Description
-                    });
-                }
-
-                return Ok(pointsOfInterestForCityResults);
+                return Ok(_mapper.Map<IEnumerable<PointOfInterestDto>>(pointsOfInterestForCity));
 
             }
             catch (Exception ex)
@@ -88,14 +76,7 @@ namespace CityGuide.API.Controllers
                 return NotFound();
             }
 
-            var pointOfInterestResult = new PointOfInterestDto()
-            {
-                Id = pointOfInterest.Id,
-                Name = pointOfInterest.Name,
-                Description = pointOfInterest.Description
-            };
-
-            return Ok(pointOfInterestResult);
+            return Ok(_mapper.Map<PointOfInterestDto>(pointOfInterest));
         }
 
         [HttpPost]
